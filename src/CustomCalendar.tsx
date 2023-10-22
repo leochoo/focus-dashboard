ac3f9f62217fc0735b276df8805a29e60506686d
leochoo <leochoodev@gmail.com>
2023-10-22 15:33:12 +0900

    trying to make it work with multiple date arrays but not good.

--- src/CustomCalendar.tsx
+++ src/CustomCalendar.tsx
@@ -1,16 +1,60 @@
+import dayjs from "dayjs";
 import React, { useState } from "react";
+import { Box, Indicator } from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
-import { Box } from "@mantine/core";
 
 const CustomCalendar = () => {
-  const [value, setValue] = useState<Date[]>([]);
+  // list of selected dates
+  const [selected, setSelected] = useState<Date[]>([]);
+  // list of indicator dates
+  const [indicated, setIndicated] = useState<Date[]>([]);
+
+  const handleSelected = (inputDate: Date) => {
+    const isSelected = selected.some((s) => dayjs(inputDate).isSame(s, "date"));
+
+    const isIndicated = selected.some((s) =>
+      dayjs(inputDate).isSame(s, "date")
+    );
+
+    if (isSelected) {
+      const newSelected = selected.filter(
+        (s) => !dayjs(inputDate).isSame(s, "date")
+      );
+      setSelected(newSelected);
+      setIndicated([...indicated, inputDate]);
+    } else if (isIndicated) {
+      const newIndicated = indicated.filter(
+        (s) => !dayjs(inputDate).isSame(s, "date")
+      );
+      setIndicated(newIndicated);
+    } else {
+      setSelected([...selected, inputDate]);
+    }
+  };
+
+  const renderDay = (date: Date) => {
+    const day = date.getDate();
+    const isDayIndicated = indicated.some((ind) =>
+      dayjs(date).isSame(ind, "date")
+    );
+
+    return (
+      <Indicator size={6} color="red" offset={-2} disabled={!isDayIndicated}>
+        <div>{day}</div>
+      </Indicator>
+    );
+  };
+
   return (
     <Box>
       <DatePicker
         type="multiple"
         numberOfColumns={2}
-        value={value}
-        onChange={setValue}
+        value={selected}
+        renderDay={renderDay}
+        getDayProps={(date) => ({
+          onClick: () => handleSelected(date),
+        })}
       />
       ;
     </Box>
